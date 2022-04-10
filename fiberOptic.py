bc084521c5e96387b8cdcb8e7b122ed3ba2b2046
Jeffrey Lyons <jeffreylyons@outlook.com>
2022-04-10 18:24:42 -0400

    commit 1

---
+++ fiberOptic.py
@@ -0,0 +1,10 @@
+print('Welcome to Lyons Fiber Optic')
+
+companyName = input('What is your company name?')
+
+fiberFeet = input('Fiber optic feet to be installed?')
+
+totalCost = int(fiberFeet) * .87
+
+print(f"{companyName} owes ${totalCost}")
+
